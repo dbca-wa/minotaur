a48057c164e2522888e6aa1252975eb197cf1472
Ashley Felton <ashley@ropable.com>
2022-08-24 13:46:11 +0800

    Config for admin site.

--- minotaur/urls.py
+++ minotaur/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.urls import path, include
 
+
+admin.site.site_header = 'Minotaur monitoring service admin'
+admin.site.index_title = 'Minotaur monitoring service'
+admin.site.site_title = 'Minotaur'
+
+
 urlpatterns = [
     path('jobs/', include('jobsy.urls')),
     path('admin/', admin.site.urls),
